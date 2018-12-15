19c7211ef92ca48689a4d08bfb96d4bf3a173165
Daneris Cruz <daneriscruz@Daneriss-MacBook-Air.local>
2018-12-15 12:12:20 -0500

    checkpoint 2755 complete

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
  import { BrowserRouter } from 'react-router-dom';
  import './index.css';
  import App from './App';
- import registerServiceWorker from './registerServiceWorker';
+ import registerServiceWorker from './serviceWorker';
 
 
  ReactDOM.render(
