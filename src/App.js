15bc778cf66ef50fb0766da1b268d52d6d9f0b2c
Daneris Cruz <daneriscruz@Daneriss-MacBook-Air.local>
2018-12-16 19:56:34 -0500

    checkpoint 2758 bloc jams react components album :Completed

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
         <main>
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
-          <Route path="/album" component={Album} />
+          <Route path="/album/:slug" component={Album} />
         </main>
       </div>
     );
@@ -29,4 +29,5 @@ class App extends Component {
 
 export default App;
 
+//comment
 //comment
