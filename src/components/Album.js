fcd6b4a2f1de2413574dcc2e2e814e8d3ba7cc85
Daneris Cruz <daneriscruz@Daneriss-MacBook-Air.local>
2018-12-20 17:28:29 -0500

    checkpoint 2761 player-bar

--- src/components/Album.js
+++ src/components/Album.js
@@ -13,6 +13,8 @@ class Album extends Component {
     this.state = {
       album: album,
       currentSong: album.songs[0],
+      currentTime: 0,
+      duration: album.songs[0].duration,
       isPlaying: false,
       hover: false
     };
@@ -20,6 +22,26 @@ class Album extends Component {
     this.audioElement = document.createElement("audio");
     this.audioElement.src = album.songs[0].audioSrc;
   }
+
+    componentDidMount() {
+      this.eventListeners ={
+      timeupdate: e => {
+        this.setState({currentTime: this.audioElement.currentTime});
+      },
+      durationchange: e => {
+        this.setState({duration: this.audioElement.duration})
+      }
+    };
+    this.audioElement.addEventListener("timeupdate", this.eventListeners.timeupdate)
+    this.audioElement.addEventListener("durationchange", this.eventListeners.durationchange)
+    }
+
+
+    componentWillUnmount() {
+      this.audioElement.src = null;
+      this.audioElement.removeEventListener("timeupdate", this.eventListeners.timeupdate)
+      this.audioElement.removeEventListener("durationchange", this.eventListeners.durationchange)
+    }
 
     play() {
       this.audioElement.play();
@@ -72,6 +94,21 @@ class Album extends Component {
       this.play();
     }
 
+    handleTimeChange(e) {
+         const newTime = this.audioElement.duration * e.target.value;
+         this.audioElement.currentTime = newTime;
+         this.setState({ currentTime: newTime });
+       }
+
+       //formatTime(timeSecs) {
+         //let result = timeSecs/60;
+         //let minutes = result.toPrecision(2);
+
+        //const word = timeSecs.toString();
+        //change to format M:SS
+       //}
+       //return minutes
+    // }
 
     hoverOn(index) {
       console.log("hovering button")
@@ -163,9 +200,12 @@ class Album extends Component {
         <PlayerBar
           isPlayiing={this.state.isPlaying}
           currentSong={this.state.currentSong}
+          currentTime={this.state.currentSong}
+          duration={this.audioElement.duration}
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
           handlePrevClick={() => this.handlePrevClick()}
           handleNextClick={() => this.handleNextClick()}
+          handleTimeChange={(e) => this.handleTimeChange(e)}
           />
 
       </section>
